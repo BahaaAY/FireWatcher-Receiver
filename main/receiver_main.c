d36704bee8369cf532f4fbf748eeaf35d76949e9
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-05-12 21:10:44 +0300

    AP mode

--- main/receiver_main.c
+++ main/receiver_main.c
@@ -16,6 +16,13 @@
 #include "esp_lvgl_port.h"
 #include "lvgl.h"
 
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "nvs_flash.h"
+
+
 // LORA PINS
 #define SCK 5
 #define MISO 19
@@ -210,9 +217,71 @@ void setupOled() {
   ESP_LOGI(TAG, "Display LVGL Scroll Text");
 }
 
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data) {
+  TAG = "WIFI_EVENT";
+  if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+    wifi_event_ap_staconnected_t *event =
+        (wifi_event_ap_staconnected_t *)event_data;
+    ESP_LOGI(TAG, "station " MACSTR " join, AID=%d", MAC2STR(event->mac),
+             event->aid);
+  } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+    wifi_event_ap_stadisconnected_t *event =
+        (wifi_event_ap_stadisconnected_t *)event_data;
+    ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d", MAC2STR(event->mac),
+             event->aid);
+  }
+}
+
+void setupAP(void) {
+
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_ap();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
+
+  wifi_config_t wifi_config = {
+      .ap =
+          {
+              .ssid = "TEST",
+              .ssid_len = strlen("TEST"),
+              .channel = 1,
+              .password = "123456789",
+              .max_connection = 4,
+#ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
+              .authmode = WIFI_AUTH_WPA3_PSK,
+              .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+#else /* CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT */
+              .authmode = WIFI_AUTH_WPA2_PSK,
+#endif
+              .pmf_cfg =
+                  {
+                      .required = true,
+                  },
+          },
+  };
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+}
 void app_main() {
   gpio_set_direction(RECEIVER_LED, GPIO_MODE_OUTPUT);
-
+  setupAP();
   setupOled();
 
   ESP_LOGI(TAG, "starting up");
